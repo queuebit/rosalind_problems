ddde1339c410cd02cc87308353548c205f5164e8
queuebit <queuebit10648@gmail.com>
2013-01-31 08:27:19 -0500

    Completed PERM

--- PERM/ROS_PERM.rb
+++ PERM/ROS_PERM.rb
@@ -6,7 +6,11 @@ def factorial (n)
 	(1..n).inject {|s, n| s*n}
 end
 
-# See http://flori.github.com/permutation/
+# Also see http://flori.github.com/permutation/
+def perm_sets (n)
+  ps = (1..n).to_a.permutation.to_a
+  ps.each {|a| print a.join(' '); puts;};
+end
 
 puts factorial x
-show_perms x
+perm_sets x
