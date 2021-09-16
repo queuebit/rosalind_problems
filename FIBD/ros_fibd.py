ce13377c074c3bf23b576b5a03c44ed977923bfa
queuebit <queuebit10648@gmail.com>
2021-09-16 17:00:42 -0400

    Add math-y solution that does not require building full model

--- FIBD/ros_fibd.py
+++ FIBD/ros_fibd.py
@@ -20,6 +20,8 @@ print(f"m: {m}")
 fib = [1, 1, 2]
 #pop = [[0], [1], [0, 2]]
 last_pop = [0,2]
+pop_crit = [[1,0,0], [0,1,0], [1,1,0]]
+print(pop_crit)
 
 for x in range(3,n):
     ### A
@@ -41,21 +43,29 @@ for x in range(3,n):
     #prev_month = pop[-1]
     this_month = []
 
-    for r in last_pop:
-        if r == 0:
-            this_month.append(1)
-        elif r == m - 1:
-            this_month.append(0)
-        else:
-            this_month.append(0)
-            this_month.append(r+1)
+    #for r in last_pop:
+    #    if r == 0:
+    #        this_month.append(1)
+    #    elif r == m - 1:
+    #        this_month.append(0)
+    #    else:
+    #        this_month.append(0)
+    #        this_month.append(r+1)
+    ln, lm, lr = pop_crit[-1]
+    math = ln + lm*2 - lr
+    tn = lm
+    tm = ln + lm - lr
+    tr = pop_crit[-(m-1)][0] if len(pop_crit) >= (m - 1) else 0
+    pop_crit.append([tn, tm, tr])
 
     #pop.append(this_month)
     last_pop = this_month
 
     #print(f"prev month MON:{x} rabbits: {len(prev_month)} -- {prev_month}")
-    #print(f"this month MON:{x+1} rabbits: {len(this_month)} -- {this_month}")
-    fib.append(len(this_month))
+    #print(f"MON:{x+1} | NEW: {this_month.count(0)} | MATURE: {len(this_month) - this_month.count(0)} | RETIRING: {this_month.count(m - 1)} [rabbits]: {len(this_month)} -- {this_month}")
+    #fib.append(len(this_month))
+    #print(f"MATH: {math} crit: {pop_crit[-1]}")
+    fib.append(math)
 
     #pop_rab = rab_a + rab_b - ret_rab
     #print(m, rab_a, rab_b, ret_rab)
